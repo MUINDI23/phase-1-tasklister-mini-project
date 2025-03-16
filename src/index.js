198bcac4fd943da1cfb680e5e5566e4a9f49f142
MUINDI23 <bbamnrb203915@spu.ac.ke>
2025-03-16 16:02:51 +0300

    Complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById('create-task-form');
+  const taskInput = document.getElementById('new-task-description');
+  const taskList = document.getElementById('tasks');
+  const taskPriority = document.getElementById('task-priority');
+
+  taskForm.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+
+    const taskDescription = taskInput.value.trim();
+
+    if (taskDescription !== '') {
+      const listItem = document.createElement('li');
+      listItem.textContent = taskDescription;
+      listItem.classList.add(taskPriority.value);
+      taskList.appendChild(listItem);
+      taskInput.value = ''; //clear input field
+
+      listItem.addEventListener('click', () => {
+        listItem.remove();
+      });    
+    }
+  });
 });
